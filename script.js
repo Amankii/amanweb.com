0e83f83acdc96da88c749a746a1ba40a85d1acc4
Arman__Yhyaa <111435138+Amankii@users.noreply.github.com>
2024-05-18 15:59:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -28,4 +28,65 @@ function display(result){
     resultsBox.innerHTML = "<ul>" + content + "</ul>"; 
 }
 */
+/*body#5a0834*/
+/*btn#9d0b59*/
+document.addEventListener('DOMContentLoaded', () => {
 
+    const webApp = document.querySelector('#webapp');
+    const pcApp = document.querySelector('#pcapp');
+    const webtn = document.querySelector('#web');
+    const pcbtn = document.querySelector('#pc');
+
+    webtn.onclick = () => {
+
+        webApp.style.display = 'flex';
+        pcApp.style.display = 'none';
+
+
+    };
+
+    pcbtn.onclick = () => {
+
+        pcApp.style.display = 'flex';
+        webApp.style.display = 'none';
+
+    };
+
+    /*    send    ----------------------------*/
+    
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyIkcr6ygoSOxMz-YesYGU84sppGMraOD12lgZuWenyCkTeEgLYwi2jpuyO5V6i5rWanA/exec'
+    const form = document.forms['submit-to-google-sheet'];
+    const input = document.querySelector('#z');
+    const btn = document.querySelector('.btn');
+    const msg = document.querySelector('#msg');
+  
+    /*---- Disabled ------*/
+    btn.disabled = true;
+    input.onkeyup = () => {
+
+        if(input.value.length > 0){
+            btn.disabled = false;
+        } else {
+            btn.disabled = true;
+        }
+    };
+
+    
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+
+            msg.innerHTML = 'Message sent successfully';
+
+            setTimeout(() => {
+                msg.innerHTML = '';
+            }, 5000);
+            form.reset();
+            btn.disabled = true;
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+  
+
+});
